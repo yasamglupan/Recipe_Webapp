eed3cc32d267f34edabf28f5ae9bb37e0da9561c
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-09-15 14:30:33 +1200

    added a about page

--- recipe.py
+++ recipe.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, abort
 import sqlite3
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html", title = "Home")
+    return render_template("home.html", title="Home")
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -21,7 +21,9 @@ def contact():
 
 @app.route('/about')
 def about():
-    return render_template("about.html", title = "About")
+    return render_template("about.html", title="About")
+
+
 
 @app.route('/all_recipes')
 def all_recipes():
@@ -29,42 +31,51 @@ def all_recipes():
     cur = conn.cursor()
     cur.execute('SELECT * FROM Recipe')
     results = cur.fetchall()
-    return render_template('all_recipes.html', results = results)
+    return render_template('all_recipes.html', results=results)
+
 
 @app.route('/recipe/<int:id>')
 def recipe(id):
     conn = sqlite3.connect('recipe.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM Recipe WHERE recipe_id=?',(id,))
+    cur.execute('SELECT * FROM Recipe WHERE recipe_id=?', (id,))
     recipe = cur.fetchone()
-    print (recipe)
-    cur.execute('SELECT name FROM Ingredients WHERE id IN(SELECT ingredient_id FROM Recipe_Ingredients WHERE recipe_id=?)',(id,))
+    if recipe is None:
+        abort(404)
+    print(recipe)
+    cur.execute('SELECT name FROM Ingredients WHERE id IN\
+        (SELECT ingredient_id FROM Recipe_Ingredients WHERE recipe_id=?)', (id, ))
     ingredients = cur.fetchall()
     cur.execute("SELECT * FROM Recipe_Ingredients WHERE recipe_id=?", (recipe[0],))
     quantity = cur.fetchall()
-    cur.execute('SELECT * FROM Review WHERE recipe_id=?',(id,))
+    cur.execute('SELECT * FROM Review WHERE recipe_id=?', (id,))
     reviews = cur.fetchall()
-    
-    return render_template('recipe.html', recipe = recipe, ingredients = ingredients,reviews = reviews,quantity=quantity)
+    return render_template('recipe.html',
+                            recipe=recipe,
+                            ingredients=ingredients,
+                            reviews=reviews,
+                            quantity=quantity)
 
 
-@app.route('/recipes', methods=['POST']) 
-def add_recipes(): 
-    conn = sqlite3.connect('recipe.db') 
-    name = request.form['name'] 
-    review = request.form['review'] 
+@app.route('/recipes', methods=['POST'])
+def add_recipes():
+    conn = sqlite3.connect('recipe.db')
+    name = request.form['name']
+    review = request.form['review']
     rating = request.form['rating']
-    recipe_id = request.form['recipe_id'] 
-    conn = sqlite3.connect('recipe.db') 
-    cursor = conn.cursor() 
-    cursor.execute('INSERT INTO Review (name, rating, review, recipe_id) VALUES (?, ?, ?, ?)', (name, rating, review, recipe_id)) 
-    conn.commit() 
+    recipe_id = request.form['recipe_id']
+    conn = sqlite3.connect('recipe.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Review (name, rating, review, recipe_id) \
+        VALUES (?, ?, ?, ?)', (name, rating, review, recipe_id))
+    conn.commit()
     return redirect(url_for("recipe", id=recipe_id))
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
