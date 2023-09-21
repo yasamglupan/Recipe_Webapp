c3de31ac94547f84f387162860f6f50a3e29502f
yasamglupan <petar@filipovic.nz>
2023-09-22 03:27:37 +1200

    Added all the finnishing touches the project is done

--- recipe.py
+++ recipe.py
@@ -6,23 +6,24 @@ app = Flask(__name__)
 
 def retrive_query_results(sql_statement, fetch_method="all", values=None):
     """
-    This is a function that takes a sql statment, opens a connection, runs the sql query, and
+    This is a function that takes a sql statement, opens a connection, runs the sql query, and
     either commits or fetches the results and returns the results
 
+
     :sql_statment: This is the sql query
-    :fetch_method: This confuirms if to fetch one, fetch all, or fetch no results
-    :values: If values are passed exicute with the values
+    :fetch_method: This confirms if to fetch one, fetch all, or fetch no results
+    :values: If values are passed execute with the values
     :return: returns the results of the query sql_statment
     """
     conn = sqlite3.connect('recipe.db')
     cur = conn.cursor()
-    if values:  # if values are passed exicute with the values
+    if values:  # if values are passed execute with the values
         cur.execute(sql_statement, values)  # Pass values as a tuple
     else:
         cur.execute(sql_statement)
     if fetch_method == "one":  # Fetch one
         result = cur.fetchone()
-    elif fetch_method == "none":  # Commit
+    elif fetch_method == "none":  # if fetch method is none commit
         conn.commit()
         result = None  # Return None when no fetch is needed
     else:  # Fetch all
@@ -41,11 +42,11 @@ def home():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
+        # Sets all the variables to the information inputed
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
-        # Process the contact form data, such as sending an email
-        # In a real application, you would typically handle the form data here
+        # SQL staement to insert the contact information into the database
         sql_statement = "INSERT INTO Contact (name, email, message)\
                         VALUES (?, ?, ?)"
         retrive_query_results(sql_statement, "none", (name, email, message))
@@ -71,35 +72,47 @@ def all_recipes():
 # Route for individual recipes page
 @app.route('/recipe/<int:id>')
 def recipe(id):
-    sql_statement = "SELECT * FROM Recipe WHERE recipe_id={}".format(id)
+
+    """
+    This function passes 3 sql quaries with a recipe id and returns all the
+    data so that it can be displayed on the recipe page
+
+    :id: This is the id of the recipe that we are fetching data for
+    :return: Returns the general recipe information 
+             The ingredients with the quantity
+             Any revews written on the recipe 
+    """
+
     # Execute an SQL query to retrieve a specific recipe by ID
+    sql_statement = "SELECT * FROM Recipe WHERE recipe_id={}".format(id)
     recipe = retrive_query_results(sql_statement, "one")
     if recipe is None:
         abort(404)  # Raise a 404 error if the recipe doesn't exist
-    # Execute an SQL query to retrieve ingredient names for the recipe
+    # Execute an SQL query to retrieve ingredient names and quantityfor the recipe
     sql_statement = "SELECT Ingredients.name, Recipe_Ingredients.quantity\
                     FROM Ingredients \
                     JOIN Recipe_Ingredients ON Ingredients.id = Recipe_Ingredients.ingredient_id\
                     WHERE Recipe_Ingredients.recipe_id = {}".format(id)
     ingredients = retrive_query_results(sql_statement)
-    # Execute an SQL query to retrieve reviews for the recipe
+    # Execute an SQL query to retrieve reviews for the recipe id
     sql_statement = "SELECT * FROM Review WHERE recipe_id={}".format(id)
     reviews = retrive_query_results(sql_statement)
     return render_template('recipe.html',
-                            recipe=recipe,
-                            ingredients=ingredients,
-                            reviews=reviews)
+                           recipe=recipe,
+                           ingredients=ingredients,
+                           reviews=reviews)
 
 
 # Route for adding reviews
 @app.route('/recipes', methods=['POST'])
 def add_recipes():
+    # Sets all the variables to the information inputed
     name = request.form['name']
     review = request.form['review']
     rating = request.form['rating']
     recipe_id = request.form['recipe_id']
     # Execute an SQL query to insert a new review into the Review table
-    sql_statement = "INSERT INTO Review (name, rating, review, recipe_id)\
+    sql_statement = "INSERT INTO Review(name, rating, review, recipe_id)\
                     VALUES (?, ?, ?, ?)"
     retrive_query_results(sql_statement, "none", (name, rating, review, recipe_id))
     return redirect(url_for("recipe", id=recipe_id))
